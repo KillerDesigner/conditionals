1e64423ea45a848827bdaab0808d01809d2310a3
Feather <lisafeather@gmail.com>
2013-09-20 14:27:58 -0700

    add more exercises

--- drinking.rb
+++ drinking.rb
@@ -1,12 +1,6 @@
 puts "How old are you?"
 age = gets.chomp.to_i
-legal_age = 21
-response = gets.chomp
 
-if age < 21
-	puts "you can't come in"
-elsif response == "17 plus here is $20"
-	puts "sure, come in Kid"
-else
-	puts "come on in"
-end
+# create a bouncer bot that can
+# verify if someone can enter the bar!
+# hint: use conditional logic
---
+++ grades.rb
@@ -0,0 +1,11 @@
+puts "What grade will you get?"
+grade = gets.chomp
+
+# use a conditional statement to 
+# evaluate the person's grade on the following scale:
+#
+# A = "excellent"
+# B = "well done"
+# C = "passing"
+# D = "derp"
+# F = "fail!"

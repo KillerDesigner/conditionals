ef1b8e9bb401ac0f1e964ca3b60a619f271b4988
Feather <lisafeather@gmail.com>
2013-09-23 15:50:11 -0700

    add lab

---
+++ conditional_lab.rb
@@ -0,0 +1,24 @@
+# in irb, evaluate the following:
+4 < 5
+4 == 5
+1 <= 2
+
+blue = true
+red = false
+
+blue || red
+blue && blue
+red && red
+red && blue
+
+# If all that was too easy, please try this:
+# Suddenly you find yourself in the negative utopia
+# that is 1984! Write a code snippet to give a result if
+# 2 + 2 == 5 and another result if that does not evaluate to true
+
+if # your code here
+
+else
+
+end
+
